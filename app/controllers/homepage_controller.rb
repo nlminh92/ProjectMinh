81305ce19c02192806fbbee733e40f70349db85b
nlminh92 <kunsjeunh4n@gmail.com>
2017-11-21 14:28:40 +0100

    Deploy 3

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,6 +2,6 @@ class HomepageController < ApplicationController
 
 
   def index
-    @users = User.where(activated: true).where("brand_id is not null")
+    @users = User.where(activated: true).where("showroom_id is not null")
   end
 end
