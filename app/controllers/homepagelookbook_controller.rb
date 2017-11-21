93e78b177bd3fe4b3dd3af4101b3efd8ae72f398
Tran Huu Minh <minhth1905@gmail.com>
2017-11-21 21:28:23 +0700

    add lookbook

--- app/controllers/homepagelookbook_controller.rb
+++ app/controllers/homepagelookbook_controller.rb
@@ -1,5 +1,7 @@
 class HomepagelookbookController < ApplicationController
-    def index
-        @collections = Collection.where("lookbook is not null")
-      end
+
+    def show
+      @collections = Collection.where("lookbook is not null")
+                                .where(brand_id: params[:id])
+    end
 end
